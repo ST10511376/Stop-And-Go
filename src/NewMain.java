5631c1594d3e09c5b7046f8aa4d911109c0567d3
ST10511376 <ST10511376@myemeris.edu.za>
2026-02-24 12:35:04 +0200

    First Commit/Submission

---
+++ src/NewMain.java
@@ -0,0 +1,36 @@
+/**                 
+ * creating a comment
+ */ 
+/**
+ * 
+ * @author ST10511376
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // Writing the code for stop and go signs
+        
+        System.out.println("Hello\nWelcome");
+        System.out.print("      ********");  
+        System.out.println("                        XXXXXXXX");
+        System.out.print("    *         *                    "); System.out.println(" X          X");
+        System.out.print("   *  S T O P  *    "); System.out.println("               X     GO     X");
+        System.out.print("    *         *     ");System.out.println("                X          X");
+        System.out.print("      ********"); System.out.println("                        XXXXXXXX");
+        System.out.print("         **"); System.out.println("                              XX");
+        System.out.print("         **");System.out.println("                              XX");
+        System.out.print("         **");System.out.println("                              XX");
+        System.out.print("         **");System.out.println("                              XX");
+        System.out.print("        ****");System.out.println("                            XXXX");
+        System.out.print("     **********");System.out.println("                      XXXXXXXXXX");
+        System.out.println("|--------------------------------------------------|");
+        System.out.println("|--------------------------------------------------|");
+        
+    } 
+    
+    
+}
